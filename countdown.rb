f2f559ac62a10e1c7540c376981a565ac70f7b9e
Alberto <ajsaenz1091@gmail.com>
2021-06-24 15:35:53 -0400

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(num)
     while num >= 1
-        p "#{num} SECOND(S)!"
+        puts "#{num} SECOND(S)!"
         num -= 1
     end
     "HAPPY NEW YEAR!"
@@ -11,7 +11,7 @@ end
 def countdown_with_sleep(num)
     while num >= 1
         sleep(1)
-        p "#{num} SECOND(S)!"
+        puts "#{num} SECOND(S)!"
         num -= 1
     end
     "HAPPY NEW YEAR!"
